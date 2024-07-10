e6645f4957c1c9035c373ac515b7ed6fd16a4d24
Gabriel Ferreira Amorim <gabrielamorim15436@gmail.com>
2024-07-09 22:29:45 -0300

    Finalizando

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,179 @@
+"use client";
+
 import Image from "next/image";
+import { BsFillMoonStarsFill } from "react-icons/bs";
+import { AiFillLinkedin, AiFillInstagram, AiFillGithub } from "react-icons/ai";
+import eu from "../../public/Eu.png";
+import design from "../../public/design.png";
+import code from "../../public/code.png";
+import codificador from "../../public/Codificador.jpeg";
+import cinemark from "../../public/Cinemark.png";
+import mapa from "../../public/Mapa.jpeg";
+import conectavel from "../../public/Conectavel.jpeg";
+import { useState } from "react";
 
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
+        <section className="min-h-screen">
+          <nav className="py-10 mb-12 flex justify-between">
+            <h1 className="text-xl font-burtons dark:text-white">@Gabriel.fav</h1>
+            <ul className="flex items-center">
+              <li>
+                <BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className="cursor-pointer text-2xl dark:text-white" />
+              </li>
+              <li>
+                <a
+                  className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"
+                  href="#"
+                >
+                  Currículo
+                </a>
+              </li>
+            </ul>
+          </nav>
+          <div className="text-center p-10">
+            <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-teal-300">
+              Gabriel Ferreira Amorim
+            </h2>
+            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">
+              Desenvolvedor Fullstack.
+            </h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white">
+              Desenvolvedor empenhado em construir o software de melhor
+              qualidade. Tenho facilidade em aprender rapidamente novas
+              tecnologias e conceitos, o que me permite adaptar-me
+              eficientemente a diferentes ambientes de trabalho e projetos.
+            </p>
+          </div>
+          <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600">
+            <AiFillInstagram />
+            <AiFillLinkedin />
+            <AiFillGithub />
+          </div>
+          <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
+            <Image src={eu} layout="fill" objectFit="cover" />
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Projetos anteriores</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              Olá, sou Gabriel Ferreira Amorim, residente em São Paulo, com 19
+              anos de idade, imerso no mundo da tecnologia. Iniciei minha
+              jornada educacional na ETEC de Guaianazes, onde me formei em{" "}
+              <span className="text-teal-500">
+                técnico de desenvolvimento de sistemas
+              </span>
+              , e atualmente estou cursando desenvolvimento fullstack Java no
+              Instituto PROA.{" "}
+              <span className="text-teal-500">
+                Minha paixão reside no desenvolvimento back-end e na gestão de
+                bancos de dados
+              </span>
+              , reconhecendo a importância dessas áreas no cerne da inovação
+              tecnológica.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              Estou constantemente em busca de aprimoramento e desafios que me
+              permitam expandir meus horizontes neste fascinante universo
+              tecnológico.
+            </p>
+          </div>
+          <div className="lg:flex gap-10 justify-center">
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+              <div className="w-auto flex justify-center">
+                <Image src={design} width={100} height={100} />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Básico em Prototipação
+              </h3>
+              <p>Criando designs elegantes adequados à sua teoria de design.</p>
+              <h4 className="py-4 text-teal-600">As ferramentas que utilizo</h4>
+              <p className="text-gray-800 py-1">Figma</p>
+              <p className="text-gray-800 py-1">Photoshop</p>
+            </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white">
+              <div className="w-auto flex justify-center">
+                <Image src={code} width={100} height={100} />
+              </div>
+              <h3 className="text-lg font-medium pt-8 pb-2">
+                Linguagens e Frameworks
+              </h3>
+              <p>
+                Desenvolvendo aplicações com o que há de mais recente no
+                mercado.
+              </p>
+              <h4 className="py-4 text-teal-600">As ferramentas que utilizo</h4>
+              <p className="text-gray-800 py-1">Javascript</p>
+              <p className="text-gray-800 py-1">React</p>
+              <p className="text-gray-800 py-1">Java</p>
+              <p className="text-gray-800 py-1">Spring boot</p>
+            </div>
+          </div>
+        </section>
+        <section>
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              Ao longo da minha carreira, participei de{" "}
+              <span className="text-teal-500">diversos projetos</span> que
+              exemplificam minhas habilidades e meu compromisso com a
+              excelência. Cada projeto é uma{" "}
+              <span className="text-teal-500">
+                oportunidade de aplicar meus conhecimentos
+              </span>{" "}
+              e de aprender algo novo, sempre com o objetivo de superar as
+              expectativas dos clientes e usuários.
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-white">
+              Eu busco sempre aplicar o máximo de tudo que aprendi em meus
+              projetos.
+            </p>
+          </div>
+          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={codificador}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={cinemark}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={mapa}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+            <div className="basis-1/3 flex-1">
+              <Image
+                src={conectavel}
+                className="rounded-lg object-cover"
+                width={"100%"}
+                height={"100%"}
+                layout="responsive"
+              />
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
